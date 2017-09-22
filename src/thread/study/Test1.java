70f55e3cf3dd203dfa5ab487efb97146d8ccc20d
ylwang <530948034@qq.com>
2017-09-22 17:25:13 +0800

    有四个线程1、2、3、4。线程1的功能就是输出1，线程2的功能就是输出2，以此类推.........现在有四个文件ABCD。初始都为空。现要让四个文件呈如下格式：
    	A：1 2 3 4 1 2....
    
    	B：2 3 4 1 2 3....
    
    	C：3 4 1 2 3 4....
    
    	D：4 1 2 3 4 1....

--- src/thread/study/Test1.java
+++ src/thread/study/Test1.java
@@ -45,11 +45,8 @@ public class Test1 {
 		Thread t4 = new Thread(new Thread4(),"线程4");
 
 		t1.start();
-		//t1.join();
 		t2.start();
-		//t2.join();
 		t3.start();
-		//t3.join();
 		t4.start();
 
 		cdl.await();
